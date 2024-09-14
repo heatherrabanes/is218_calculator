f40c67350c44d73647bec6f3161e90b2d5afe06b
Heather Rabanes <heatherrabanes@gmail.com>
2024-09-13 20:30:35 -0400

    fixed pylint problems

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,5 +2,5 @@
 from app.main import addition
 
 def test_addition():
-        '''Addition function'''
-        assert addition(1,1)  == 2
+    '''Addition function'''
+    assert addition(1,1)  == 2
